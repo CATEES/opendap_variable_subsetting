419201f5944c9d72373de8fabfad759157061207
Paul Madden <pmadden@nsidc.org>
2017-01-03 08:41:41 -0700

    Check for env existence before removing

--- develop.sh
+++ develop.sh
@@ -2,7 +2,7 @@
 
 set -e
 env=opendap_variable_subsetting
-conda env remove -q -y -n $env
+conda info --envs | grep -q "^$env" && conda env remove -q -y -n $env
 conda env create -f develop.yml
 source activate $env
 jupyter notebook --notebook-dir=. --ip=0.0.0.0
